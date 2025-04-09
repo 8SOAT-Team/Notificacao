1a3875505e01418327b6680461410ba0102b8d61
Fernanda Vecchiati Beato <feehvecch@gmail.com>
2025-04-08 21:59:01 -0300

    Update Program.cs

--- Notificacao.Api/Program.cs
+++ Notificacao.Api/Program.cs
@@ -2,6 +2,9 @@ using Notificacao;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var sendGridApiKey = builder.Configuration.GetValue<string>("SendGrid:ApiKey");
+builder.Services.AddSingleton<ISendGridClient>(new SendGridClient(sendGridApiKey));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
